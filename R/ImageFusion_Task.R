bd2eaada47786202f56f0244856ddd7fe92f58ab
JohMast <44399453+JohMast@users.noreply.github.com>
2020-08-25 11:09:31 +0200

    added option to set output filenames. Fixed typo in the assertions of res and extent.

--- R/ImageFusion_Task.R
+++ R/ImageFusion_Task.R
@@ -32,60 +32,65 @@
 #' @return A ggplot overview of the tasks (If \code{output_overview} is "true")
 #' @import assertthat ggplot2 magrittr dplyr
 #' @importFrom assertthat assert_that
+#' @importfrom raster res
 #' @export
 #' @details The function firstly seeks among the inputs for pair dates, which are dates for which have both a high resolution image and a low resolution image are available. It then splits the task into a number of self-contained \emph{jobs} which handle the fusion between these pair dates using the paired images as anchors. These jobs can also be called directly via their respective functions, see \code{method}.
 #' @author  Christof Kaufmann (C++), Johannes Mast (R)
 #' @seealso 
 #' @examples Sorry, maybe later
 
-imagefusion_task <- function(...,filenames_high,filenames_low,dates_high,dates_low,dates_pred,singlepair_mode="ignore",method="starfm",spstfm_mode="none",high_date_prediction_mode="ignore",verbose=T,output_overview=T,out_dir="Pred_Outputs"){
-
-####1: Prepare Inputs####
+imagefusion_task <- function(...,filenames_high,filenames_low,dates_high,dates_low,dates_pred,filenames_pred=NULL,singlepair_mode="ignore",method="starfm",spstfm_mode="none",high_date_prediction_mode="ignore",verbose=T,output_overview=T,out_dir="Pred_Outputs"){
+  
+  ####1: Prepare Inputs####
   
   #Check output folder
   if(ifelse(!dir.exists(out_dir), dir.create(out_dir,recursive = T), FALSE)){
     if(verbose){
       print(paste("Creating output directory:",out_dir)) ##attemo
-      }#end if verbose
+    }#end if verbose
   }else{#end if dir was created
     if(verbose){
       print(paste("Saving to output directory:",out_dir))
     }#end if verbose
+    
   }#end else
-filenames_pred <- file.path(out_dir,paste(dates_pred,".tif",sep = ""))  #Make plausible out filenames
-
-#Make sure that method is plausible
-assert_that(method %in% c("estarfm","fitfc","spstfm","starfm"))
-#Make sure that high_date_prediction_mode is plausible
-assert_that(high_date_prediction_mode %in% c("ignore","copy","force"))
-#Set singlepair policy
-#ignore: Do not even attempt to predict singlepair dates
-#mixed: Use doublepair mode when possible, and singlepair mode otherwise
-#all: Predict all dates in singlepair mode
-assert_that(singlepair_mode %in% c("ignore","mixed","all"))
-
-#Set spstfm policy
-#none: Do not save dicts and always train from scratch
-#separate: train and output separate dicts for each job
-#iterative: reuse trained dicts on successive jobs and update them iteratively
-assert_that(spstfm_mode %in% c("none","separate","iterative"))
-
-
-
-#mixed and all are only possible in fitfc and starfm
-assert_that(!(singlepair_mode =="mixed" & ( method == "estarfm" | method == "spstfm")))
-assert_that(!(singlepair_mode =="all" & ( method == "estarfm" | method == "spstfm")))
-
-##Check that resolutions match
-high_resolutions <- lapply(filenames_high, function(x){
-  res(stack(x))
-})
-low_resolutions <- lapply(filenames_high, function(x){
-  res(stack(x))
-})
-all_resolutions <- lapply(files, function(x){
-  res(stack(x))
-})
+  if(is.null(filenames_pred)){
+    filenames_pred <- file.path(out_dir,paste(dates_pred,".tif",sep = ""))  #Make plausible out filenames
+    
+  }
+  
+  #Make sure that method is plausible
+  assert_that(method %in% c("estarfm","fitfc","spstfm","starfm"))
+  #Make sure that high_date_prediction_mode is plausible
+  assert_that(high_date_prediction_mode %in% c("ignore","copy","force"))
+  #Set singlepair policy
+  #ignore: Do not even attempt to predict singlepair dates
+  #mixed: Use doublepair mode when possible, and singlepair mode otherwise
+  #all: Predict all dates in singlepair mode
+  assert_that(singlepair_mode %in% c("ignore","mixed","all"))
+  
+  #Set spstfm policy
+  #none: Do not save dicts and always train from scratch
+  #separate: train and output separate dicts for each job
+  #iterative: reuse trained dicts on successive jobs and update them iteratively
+  assert_that(spstfm_mode %in% c("none","separate","iterative"))
+  
+  
+  
+  #mixed and all are only possible in fitfc and starfm
+  assert_that(!(singlepair_mode =="mixed" & ( method == "estarfm" | method == "spstfm")))
+  assert_that(!(singlepair_mode =="all" & ( method == "estarfm" | method == "spstfm")))
+  
+  ##Check that resolutions match
+  high_resolutions <- lapply(filenames_high, function(x){
+    res(stack(x))
+  })
+  low_resolutions <- lapply(filenames_low, function(x){
+    res(stack(x))
+  })
+  all_resolutions <- lapply(c(filenames_high,filenames_low), function(x){
+    res(stack(x))
+  })
 
 
 if(!all(sapply(high_resolutions, identical, high_resolutions[[1]]))){
@@ -106,7 +111,7 @@ high_extents <- lapply(filenames_high, function(x){
 low_extents <- lapply(filenames_high, function(x){
   extent(stack(x))
 })
-all_extents <- lapply(files, function(x){
+all_extents <- lapply(c(filenames_high,filenames_low), function(x){
   extent(stack(x))
 })
 
@@ -154,34 +159,34 @@ all <- full_join(x = high_df,y = low_df,by=c("date")) %>%
   )) %>% ungroup
 
 if(singlepair_mode=="ignore"){
-    all <- all %>% 
-      mutate(interval_pairs = cut(date,breaks = c(-Inf,pairs$date,Inf),right = T,include.lowest = T)) %>% 
-      mutate(interval_ids = cut(date,breaks = c(-Inf,pairs$date,Inf),labels = F,right = T,include.lowest = T)) %>% 
-      mutate(interval_startpair = suppressWarnings(apply(cbind(lower = as.numeric( sub("\\((.+),.*", "\\1", interval_pairs) ),
-                                          upper = as.numeric( sub("[^,]*,([^]]*)\\]", "\\1", interval_pairs))), 1, function(x) min(x[!(is.infinite(x)|is.na(x))])))) %>% 
-      mutate(interval_endpair = suppressWarnings(apply(cbind(lower = as.numeric( sub("\\((.+),.*", "\\1", interval_pairs) ),
-                                          upper = as.numeric( sub("[^,]*,([^]]*)\\]", "\\1", interval_pairs))), 1, function(x) max(x[!(is.infinite(x)|is.na(x))])))) %>% 
-      group_by(interval_ids) %>%
-      mutate(interval_start = min(date)-1) %>% 
-      mutate(interval_end = max(date))%>% 
-      mutate(interval_npred = sum(predict[pred_case==1],na.rm = T)) %>% 
-      ungroup
+  all <- all %>% 
+    mutate(interval_pairs = cut(date,breaks = c(-Inf,pairs$date,Inf),right = T,include.lowest = T)) %>% 
+    mutate(interval_ids = cut(date,breaks = c(-Inf,pairs$date,Inf),labels = F,right = T,include.lowest = T)) %>% 
+    mutate(interval_startpair = suppressWarnings(apply(cbind(lower = as.numeric( sub("\\((.+),.*", "\\1", interval_pairs) ),
+                                                             upper = as.numeric( sub("[^,]*,([^]]*)\\]", "\\1", interval_pairs))), 1, function(x) min(x[!(is.infinite(x)|is.na(x))])))) %>% 
+    mutate(interval_endpair = suppressWarnings(apply(cbind(lower = as.numeric( sub("\\((.+),.*", "\\1", interval_pairs) ),
+                                                           upper = as.numeric( sub("[^,]*,([^]]*)\\]", "\\1", interval_pairs))), 1, function(x) max(x[!(is.infinite(x)|is.na(x))])))) %>% 
+    group_by(interval_ids) %>%
+    mutate(interval_start = min(date)-1) %>% 
+    mutate(interval_end = max(date))%>% 
+    mutate(interval_npred = sum(predict[pred_case==1],na.rm = T)) %>% 
+    ungroup
   
 }
 if(singlepair_mode=="mixed"){
-    all <- all %>% 
-      mutate(interval_pairs = cut(date,breaks = c(-Inf,pairs$date,Inf),right = T,include.lowest = T)) %>% 
-      mutate(interval_ids = cut(date,breaks = c(-Inf,pairs$date,Inf),labels = F,right = T,include.lowest = T)) %>%
-      mutate(interval_startpair = suppressWarnings(apply(cbind(lower = as.numeric( sub("\\((.+),.*", "\\1", interval_pairs) ),
-                                          upper = as.numeric( sub("[^,]*,([^]]*)\\]", "\\1", interval_pairs))), 1, function(x) min(x[!(is.infinite(x)|is.na(x))])))) %>% 
-      mutate(interval_endpair = suppressWarnings(apply(cbind(lower = as.numeric( sub("\\((.+),.*", "\\1", interval_pairs) ),
-                                          upper = as.numeric( sub("[^,]*,([^]]*)\\]", "\\1", interval_pairs))), 1, function(x) max(x[!(is.infinite(x)|is.na(x))])))) %>% 
-      
-      group_by(interval_ids) %>%
-      mutate(interval_start = min(date)-1) %>% 
-      mutate(interval_end = max(date))%>% 
-      mutate(interval_npred = sum(predict[pred_case==1 | pred_case==2],na.rm = T)) %>% 
-      ungroup
+  all <- all %>% 
+    mutate(interval_pairs = cut(date,breaks = c(-Inf,pairs$date,Inf),right = T,include.lowest = T)) %>% 
+    mutate(interval_ids = cut(date,breaks = c(-Inf,pairs$date,Inf),labels = F,right = T,include.lowest = T)) %>%
+    mutate(interval_startpair = suppressWarnings(apply(cbind(lower = as.numeric( sub("\\((.+),.*", "\\1", interval_pairs) ),
+                                                             upper = as.numeric( sub("[^,]*,([^]]*)\\]", "\\1", interval_pairs))), 1, function(x) min(x[!(is.infinite(x)|is.na(x))])))) %>% 
+    mutate(interval_endpair = suppressWarnings(apply(cbind(lower = as.numeric( sub("\\((.+),.*", "\\1", interval_pairs) ),
+                                                           upper = as.numeric( sub("[^,]*,([^]]*)\\]", "\\1", interval_pairs))), 1, function(x) max(x[!(is.infinite(x)|is.na(x))])))) %>% 
+    
+    group_by(interval_ids) %>%
+    mutate(interval_start = min(date)-1) %>% 
+    mutate(interval_end = max(date))%>% 
+    mutate(interval_npred = sum(predict[pred_case==1 | pred_case==2],na.rm = T)) %>% 
+    ungroup
   
 }
 if(singlepair_mode=="all"){
@@ -221,7 +226,7 @@ if(output_overview){
 }
 
 #Find jobs
-  valid_job_table <- all %>% 
+valid_job_table <- all %>% 
   group_by(interval_ids,interval_pairs,interval_start,interval_end,interval_npred,interval_startpair,interval_endpair) %>% 
   filter(sum(interval_npred)>0) %>% 
   summarise()
@@ -279,27 +284,27 @@ for(i in 1:nrow(valid_job_table)){ #For every job
     #FITFC
     if(method=="fitfc"){
       ImageFusion::fitfc_job(input_filenames = c(startpair_date$files_high,
-                                                   startpair_date$files_low,
-                                                   endpair_date$files_high,
-                                                   endpair_date$files_low,
-                                                   current_case_1$files_low),
-                               input_resolutions = c("high","low","high","low",rep("low",nrow(current_case_1))),
-                               input_dates = c(startpair,startpair,endpair,endpair,current_case_1$date),
-                               pred_dates = current_case_1$date,
-                               pred_filenames =  current_case_1$files_pred,verbose=verbose,...
+                                                 startpair_date$files_low,
+                                                 endpair_date$files_high,
+                                                 endpair_date$files_low,
+                                                 current_case_1$files_low),
+                             input_resolutions = c("high","low","high","low",rep("low",nrow(current_case_1))),
+                             input_dates = c(startpair,startpair,endpair,endpair,current_case_1$date),
+                             pred_dates = current_case_1$date,
+                             pred_filenames =  current_case_1$files_pred,verbose=verbose,...
       )
     }#end fitfc
     #STARFM
     if(method=="starfm"){
       ImageFusion::starfm_job(input_filenames = c(startpair_date$files_high,
-                                                   startpair_date$files_low,
-                                                   endpair_date$files_high,
-                                                   endpair_date$files_low,
-                                                   current_case_1$files_low),
-                               input_resolutions = c("high","low","high","low",rep("low",nrow(current_case_1))),
-                               input_dates = c(startpair,startpair,endpair,endpair,current_case_1$date),
-                               pred_dates = current_case_1$date,
-                               pred_filenames =  current_case_1$files_pred,verbose=verbose,...
+                                                  startpair_date$files_low,
+                                                  endpair_date$files_high,
+                                                  endpair_date$files_low,
+                                                  current_case_1$files_low),
+                              input_resolutions = c("high","low","high","low",rep("low",nrow(current_case_1))),
+                              input_dates = c(startpair,startpair,endpair,endpair,current_case_1$date),
+                              pred_dates = current_case_1$date,
+                              pred_filenames =  current_case_1$files_pred,verbose=verbose,...
       )
     }#end starfm
     #SPSTFM
@@ -312,8 +317,8 @@ for(i in 1:nrow(valid_job_table)){ #For every job
         if(i>1){
           LOADDICT_options = file.path(out_dir,paste0("Spstfm_dict_job",i-1))
           print(paste("Building on previously save dictionary: ",LOADDICT_options ))}
-          REUSE_options="improve"
-          SAVEDICT_options = file.path(out_dir,paste0("Spstfm_dict_job",i))
+        REUSE_options="improve"
+        SAVEDICT_options = file.path(out_dir,paste0("Spstfm_dict_job",i))
       }
       if(spstfm_mode=="none"){
         SAVEDICT_options = ""
@@ -322,14 +327,14 @@ for(i in 1:nrow(valid_job_table)){ #For every job
       }
       
       ImageFusion::spstfm_job(input_filenames = c(startpair_date$files_high,
-                                                   startpair_date$files_low,
-                                                   endpair_date$files_high,
-                                                   endpair_date$files_low,
-                                                   current_case_1$files_low),
-                               input_resolutions = c("high","low","high","low",rep("low",nrow(current_case_1))),
-                               input_dates = c(startpair,startpair,endpair,endpair,current_case_1$date),
-                               pred_dates = current_case_1$date,
-                               pred_filenames =  current_case_1$files_pred,
+                                                  startpair_date$files_low,
+                                                  endpair_date$files_high,
+                                                  endpair_date$files_low,
+                                                  current_case_1$files_low),
+                              input_resolutions = c("high","low","high","low",rep("low",nrow(current_case_1))),
+                              input_dates = c(startpair,startpair,endpair,endpair,current_case_1$date),
+                              pred_dates = current_case_1$date,
+                              pred_filenames =  current_case_1$files_pred,
                               SAVEDICT_options = SAVEDICT_options,
                               REUSE_options = REUSE_options,
                               verbose=verbose,
@@ -358,16 +363,16 @@ for(i in 1:nrow(valid_job_table)){ #For every job
     #STARFM
     if(method=="starfm"){
       ImageFusion::starfm_job(input_filenames = c(startpair_date$files_high,
-                                                 startpair_date$files_low,
-                                                 current_case_2$files_low),
-                             input_resolutions = c("high","low",rep("low",nrow(current_case_2))),
-                             input_dates = c(startpair,startpair,current_case_2$date),
-                             pred_dates = current_case_2$date,
-                             pred_filenames =  current_case_2$files_pred,verbose=verbose,...
+                                                  startpair_date$files_low,
+                                                  current_case_2$files_low),
+                              input_resolutions = c("high","low",rep("low",nrow(current_case_2))),
+                              input_dates = c(startpair,startpair,current_case_2$date),
+                              pred_dates = current_case_2$date,
+                              pred_filenames =  current_case_2$files_pred,verbose=verbose,...
       )
     }#end starfm
   }#end case2
-
+  
 } #End for every job
 
 ####4: Deal with other cases####
@@ -385,19 +390,19 @@ if(nrow(current_case_4)){
   if(verbose){cat(paste("\nBoth high and low images were found for date(s)", paste(current_case_4$date,collapse = " ")))}
   if(high_date_prediction_mode=="ignore"){
     if(verbose){cat("\nIgnoring these dates")}
-    }
+  }
   if(high_date_prediction_mode=="copy"){
     if(verbose){cat("\ncopying input files to target destination")}
     file.copy(current_case_4$files_high,current_case_4$files_pred)
-    }
+  }
   if(high_date_prediction_mode=="force"){
     if(verbose){cat("\npredicting on self for those dates")}
     for(i in 1:nrow(current_case_4)){
       self_predict(src_im = current_case_4$files_high[i],dst_im = current_case_4$files_pred[i],method=method,...)
-      }
     }
+  }
   
-  }#end case4
+}#end case4
 #Process Cases 5
 if(nrow(current_case_5)){
   if(verbose){cat(paste("\nBoth high and low images were found for date(s)", paste(current_case_5$date,collapse = " ")))}
@@ -419,5 +424,5 @@ if(nrow(current_case_5)){
 
 if(output_overview){return(p)}else{return(0)}
 
-  
+
 }#end function body
