785e3589ef7f6cc24f38ae535ebf4d6552c51cf7
JohMast <44399453+JohMast@users.noreply.github.com>
2020-09-18 15:49:45 +0200

    removed curl installation on windows

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,12 +1,11 @@
-if(getRversion() < "3.3.0") {
-  stop("Your version of R is too old. This package requires R-3.3.0 or newer on Windows.")
+if(getRversion() < "4.0.0") {
+  stop("Your version of R is too old. This package requires R-4.0.0 or newer on Windows.")
 }
 VERSION <- commandArgs(TRUE)
 
 
 # For details see: https://github.com/rwinlib/gdal2
 # For details see: https://github.com/rwinlib/netcdf
-if(getRversion() < "3.3.0") setInternet2()
 
 if(!file.exists("../windows/gdal2-2.2.3/include/gdal/gdal.h")){
   download.file("https://github.com/rwinlib/gdal2/archive/v2.2.3.zip", "lib.zip", quiet = TRUE)
@@ -22,14 +21,7 @@ if(!file.exists("../windows/netcdf-4.4.1.1-dap/include/netcdf.h")){
   unlink("lib.zip")
 }
 
-if(!file.exists("../windows/libcurl-7.59.0/include/curl/curl.h")){
-  download.file("https://github.com/rwinlib/libcurl/archive/v7.59.0.zip", "lib.zip", quiet = TRUE)
-  dir.create("../windows", showWarnings = FALSE)
-  unzip("lib.zip", exdir = "../windows")
-  unlink("lib.zip")
-}
 
-# Downloads libssh2 + dependencies
 if(!file.exists("../windows/opencv-%s/include/opencv4/opencv2/opencv.hpp")){
   download.file("https://github.com/rwinlib/opencv/archive/v4.0.1.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
