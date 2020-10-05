098487eef793010d1e5f4a1437a29c458456bb70
JohMast <johannes.mast@gmx.de>
2020-10-05 15:44:03 +0200

    Added skeleton for imginterp

---
+++ src/execture_imginterp_job.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+#include <boost/filesystem.hpp>
+#include <boost/range/adaptor/reversed.hpp>
+
+#include "optionparser.h"
+#include "GeoInfo.h"
+#include "MultiResImages.h"
+#include "utils_common.h"
+#include "fileformat.h"
+
+#include "interpolation.h"
+#include "customopts.h"
+
+
+
+
