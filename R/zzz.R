b5cbed3d88784e55a569ab944708b26e16732bd8
JohMast <44399453+JohMast@users.noreply.github.com>
2022-11-16 16:37:26 +0100

    Added pathfinding gdal

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,14 @@
 .onLoad <- function(libname, pkgname) {
-  if(Sys.info()["sysname"]=="Windows"){
-    path_to_proj <- file.path(Sys.getenv("R_LIBS_USER"),"Imagefusion/proj")
-    Sys.setenv(PROJ_LIB=path_to_proj)
+  # if(Sys.info()["sysname"]=="Windows"){
+  #   path_to_proj <- file.path(.libPaths(),"Imagefusion","proj")
+  #   Sys.setenv(PROJ_LIB=path_to_proj)
+  # }
+  
+  if (dir.exists(system.file("proj", package="gdalcubes"))) {
+    Sys.setenv("PROJ_LIB" = system.file("proj", package="gdalcubes"))
   }
+  if (dir.exists(system.file("gdal", package="gdalcubes"))) {
+    Sys.setenv("GDAL_DATA" = system.file("gdal", package="gdalcubes"))
+  }
+  
 }
